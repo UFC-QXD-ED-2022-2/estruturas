27adfa08796b73c27033758c37e735ca01764f50
Arthur Araruna <ararunaufc@gmail.com>
2022-09-14 11:52:14 -0300

    Alteração no cabeçalho para prevenir problemas com o preprocessador
    É de praxe incluir em todos os cabeçalhos o que se chama de "include guards". Eles evitam que o preprocessador entre em loop ao tentar compilar nossos arquivos, por conta da forma que ele funciona (substituição textual recursiva).
    
    Esse problema pode acontecer quando temos dois ou mais .h que, direta ou indiretamente, incluem o outro.

--- lista-sequencial/lista.h
+++ lista-sequencial/lista.h
@@ -1,3 +1,6 @@
+#ifndef QXD_ED_LISTA
+#define QXD_ED_LISTA 1
+
 #include <stdbool.h>
 
 struct lista;
@@ -11,3 +14,5 @@ int lista_remover(ListaInt *lista, unsigned int indice);
 int lista_remover_ordenado(ListaInt *lista, unsigned int indice);
 bool lista_buscar(ListaInt *lista, int elemento);
 int lista_buscar_indice_ordenado(ListaInt *lista, int elemento);
+
+#endif
