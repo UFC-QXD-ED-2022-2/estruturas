0180560c3736a59a62f495c25d61fd6fab85a151
Arthur Araruna <ararunaufc@gmail.com>
2022-09-14 11:37:49 -0300

    Inserindo declarações de "métodos públicos" no cabeçalho
    As funções que queremos que o usuário tenha acesso devem ser declaradas no cabeçalho. (Lembrando: o que não for mencionado no .h é considerado "privado").

--- lista-sequencial/lista.c
+++ lista-sequencial/lista.c
@@ -1,5 +1,5 @@
 #include <limits.h>
-#include <stdbool.h>
+#include <stdlib.h>
 
 #include "lista.h"
 
--- lista-sequencial/lista.h
+++ lista-sequencial/lista.h
@@ -1,3 +1,13 @@
+#include <stdbool.h>
 
 struct lista;
 typedef struct lista ListaInt;
+ListaInt *lista_criar();
+void lista_apagar(ListaInt *lista);
+
+void lista_inserir(ListaInt *lista, int elemento);
+void lista_inserir_ordenado(ListaInt *lista, int elemento);
+int lista_remover(ListaInt *lista, unsigned int indice);
+int lista_remover_ordenado(ListaInt *lista, unsigned int indice);
+bool lista_buscar(ListaInt *lista, int elemento);
+int lista_buscar_indice_ordenado(ListaInt *lista, int elemento);
