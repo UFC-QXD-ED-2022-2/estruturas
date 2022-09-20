d545bf6ae3f255534a1ad582a6f9faae2d8107e4
Arthur Araruna <ararunaufc@gmail.com>
2022-09-20 10:44:57 -0300

    Declara funções de Iteradores para Listas Encadeadas
    Implementaremos nas próximas aulas

--- lista-encadeada/lista-encadeada.h
+++ lista-encadeada/lista-encadeada.h
@@ -20,4 +20,14 @@ void lista_enc_remover_fim(ListaEncInt *lista);
 void lista_enc_remover_em(ListaEncInt *lista, int elemento, unsigned int indice);
 void lista_enc_remover_todos(ListaEncInt *lista, int elemento);
 
+
+struct iterador_lista_encadeada;
+typedef struct lista_encadeada ItListaEncInt;
+
+ItListaEncInt *it_lista_enc_criar(ListaEncInt *lista);
+void it_lista_enc_apagar(ListaIntIterador *it);
+bool it_lista_enc_tem_proximo(ListaIntIterador *it);
+int it_lista_enc_obter_elemento(ListaIntIterador *it);
+void it_lista_enc_avancar(ListaIntIterador *it);
+
 #endif
